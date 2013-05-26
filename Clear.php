cdc210f758d2d4bccbf65e496f415969cf7bcc3b
Sani-Lukian <tab_93@bk.ru>
2013-05-26 11:07:24 +0300

    Update Clear.php

--- Clear.php
+++ Clear.php
@@ -1,5 +1,6 @@
 <?php
-define("DBName","bd");
+header('Content-type: text/html; charset=utf-8');
+define("DBName","testbd");
 define("HostName","localhost");
 define("UserName","root");
 define("Password","");
@@ -10,9 +11,40 @@ exit;
 }
 
 mysql_select_db(DBName);
+ $r=mysql_query("drop database if exists ".DBName);
+
+ $r=mysql_query("create database ".DBName);
+mysql_select_db(DBName);
+
  $r=mysql_query('SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO"');
+if(!$r)
+{ echo "Error !<br>";
+echo mysql_error();
+}
+
  $r=mysql_query('SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO"');
+if(!$r)
+{ echo "Error !<br>";
+echo mysql_error();
+}
+
  $r=mysql_query('SET time_zone = "+00:00"');
- $r=mysql_query("CREATE TABLE IF NOT EXISTS `testbd` (  `data1` text NOT NULL,  `data2` text NOT NULL,  `data3` text NOT NULL,  `data4` text NOT NULL,  `data5` text NOT NULL,  `data6` text NOT NULL,  `data7` text NOT NULL,  `data8` text NOT NULL,  `data9` text NOT NULL,  `data10` text NOT NULL,  `data11` text NOT NULL ENGINE=InnoDB DEFAULT CHARSET=latin1;");
- $r=mysql_query("INSERT INTO `testbd` (`data1`, `data2`, `data3`, `data4`, `data5`, `data6`, `data7`, `data8`, `data9`, `data10`, `data11`) VALUES ('', '', '100', '100', '100', '1000', '100', '100', 'Ê ïîäåçäó', '1', ''),('', '', '100', '100', '100', '1000', '100', '100', 'Ê ïîäåçäó', '1', '');");
+if(!$r)
+{ echo "Error !<br>";
+echo mysql_error();
+}
+
+ $r=mysql_query("CREATE TABLE IF NOT EXISTS `testbd` (  `familiya` text NOT NULL,  `imya` text NOT NULL,  `ScreenSize` int NOT NULL,  `MemSize` int NOT NULL,  `HDDSize` int NOT NULL,  `SensScreen` int NOT NULL, videoPrice int NOT NULL, `CPUPrice` int NOT NULL,  `dostavka` int NOT NULL,  `kolichestvo` int NOT NULL,  `address` text NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=latin1;");
+if(!$r)
+{ echo "Error !<br>";
+echo mysql_error();
+}
+
+ $r=mysql_query("INSERT INTO `testbd` (`familiya`, `imya`, `ScreenSize`, `MemSize`, `HDDSize`, `SensScreen`, videoPrice, `CPUPrice`, `dostavka`, `kolichestvo`, `address`) VALUES ('', '', '100', '100', '100', '1000', '100', '100', '? ???????', '1', ''),('', '', '100', '100', '100', '1000', '100', '100', '0', '1', '');");
+if(!$r)
+{ echo "Error !<br>";
+echo mysql_error();
+}
  
+ 
+?>
